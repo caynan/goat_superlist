6ffbe75cbc9fab9468fa11e4b4e54a3febf8dd29
Caynan <caynanvls@gmail.com>
2014-11-03 10:21:27 -0500

    finished refactoring on the FT

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User enters To-Do homepage
         self.browser.get('http://localhost:8000')
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         # When hit enter, the page updates, and now the page lists
         # "1: Buy Milk"
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy Milk', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy Milk')
 
         # There is still a text box invinting the user to add another item.
         # the user enters "Drink Milk"
@@ -45,11 +47,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Drink Milk', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy Milk')
+        self.check_for_row_in_list_table('2: Drink Milk')
+
         # User see unique generated URL -- with some explanatory text
         self.fail('Finish the test!')
+
         # User visit the URL - the to-do list is still there.
 
         # user closes everything.
