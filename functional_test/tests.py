b49902d32afc75edfc130f4a10baef78c6dd6ade
Caynan <caynanvls@gmail.com>
2014-11-07 10:10:20 -0500

    Initial modification to use REST model & multiple users

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -34,29 +34,55 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
         # The user types "Buy Milk" into a text box
-        inputbox.send_keys("Buy Milk")
+        inputbox.send_keys("Buy peacock feathers")
 
         # When hit enter, the page updates, and now the page lists
         # "1: Buy Milk"
         inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('1: Buy Milk')
+        user_list_url = self.browser.current_url
+        self.assertRegex(user_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box invinting the user to add another item.
         # the user enters "Drink Milk"
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Drink Milk")
+        inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on the list
-        self.check_for_row_in_list_table('1: Buy Milk')
-        self.check_for_row_in_list_table('2: Drink Milk')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # User see unique generated URL -- with some explanatory text
-        self.fail('Finish the test!')
+        # A new user, comes along to the site.
 
-        # User visit the URL - the to-do list is still there.
+        ## We use a new browser session to make sure that no information
+        ## of the first user is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # user closes everything.
+        # New user visits the home page. There is no sign of the first user
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text)
+
+        # New user starts a new list.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # New user gets it's own unique URL
+        newuser_list_url = self.browser.current_url
+        self.assertRegex(newuser_list_url, '/lists/.+')
+        self.assertNotEqual(newuser_list_url, user_list_url)
+
+        # Again, there is no trace of first user's list
+        page_text = self.browser.find_element_by_tag('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Happy, the user closes everything sure that they'll be there tomorrow.
 
 
 if __name__ == '__main__':
